2f32bcc3844360ba0b4e1dde0ffb4d0a66d2413b
Hemant Singh <hemantrao1552004@gmail.com>
2026-06-20 18:24:12 +0530

    update

--- agritech/settings.py
+++ agritech/settings.py
@@ -156,11 +156,20 @@ SUPER_ADMIN_SECRET_CODE = os.getenv('SUPER_ADMIN_SECRET_CODE', 'AGROSTHAN2026-SU
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'technosthan.intern.3@gmail.com'
-EMAIL_HOST_PASSWORD = 'vysq dlnc irof grmb'
+
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+
+
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') 
+
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
